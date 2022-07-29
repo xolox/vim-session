xolox/vim-session

commit 8baa0ccd5979b7d87f00ec041c9fc6369ba44eff

Adding option 'g:session_ignore_special_pages'
This option ignore totally the "special pages" in the session save process
It allows special pages from plugins to auto-open on session start
without any error from opening a void buffer (e.g empty Tagbar)

autoload/xolox/session.vim
@@ -184,7 +184,9 @@ function! xolox#session#save_state(commands) " {{{2
     " output. We will fire the event ourselves when we're really done.
     call s:eat_trailing_line(lines, 'unlet SessionLoad')
     call s:eat_trailing_line(lines, 'doautoall SessionLoadPost')
-    call xolox#session#save_special_windows(lines)
+    if g:session_ignore_special_pages != 'yes'
+      call xolox#session#save_special_windows(lines)
+    endif
     if !xolox#session#include_tabs()
       " Customize the output of :mksession for tab scoped sessions.
       let buffers = tabpagebuflist()

commit b0369a4e35e53b31a412f343aa30ef09232b37ba

fix "session_ignore_special_pages" not defined error
introduced by 8baa0ccd5979b7d87f00ec041c9fc6369ba44eff

autoload/xolox/session.vim
@@ -184,7 +184,7 @@ function! xolox#session#save_state(commands) " {{{2
     " output. We will fire the event ourselves when we're really done.
     call s:eat_trailing_line(lines, 'unlet SessionLoad')
     call s:eat_trailing_line(lines, 'doautoall SessionLoadPost')
-    if g:session_ignore_special_pages != 'yes'
+    if exists("g:session_ignore_special_pages")
       call xolox#session#save_special_windows(lines)
     endif
     if !xolox#session#include_tabs()